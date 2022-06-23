72b593f4b91e6cc853b5411a3b72b3ce8c7cd6e3
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-06-23 15:46:57 +0600

    myfolio added

---
+++ myfolio/assets/js/main.js
@@ -0,0 +1,56 @@
+// ==================== MENU SHOW Y HIDDEN ========================//
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+//=========== Menu Show ==========//
+// Validate if constant exists
+if (navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+//=========== Menu Hide ==========//
+//validate if constant exists
+if (navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+//======================= Remove Menu Mobile =======================//
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//======================== Accordion Skills ========================//
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for (i=0;i<skillsContent.length;i++){
+        skillsContent[i].className = "skills_content skills_close";
+    }
+    if(itemClass === "skills_content skills_close"){
+        this.parentNode.className = "skills_content skills_open";
+    }
+}
+
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click', toggleSkills)
+})
+
+
+
+//======================== Qualification Tabs ======================//
+//========================== Servicec Modal =======================//
+//======================== Portfolio Swiper ======================//
+//========================== Testimonial ========================//
