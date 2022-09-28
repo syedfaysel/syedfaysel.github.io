d149d01b9ad623e522428b2a445496639559dc90
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-09-29 00:35:01 +0600

    opencourseware link added

--- myfolio/assets/js/main.js
+++ myfolio/assets/js/main.js
@@ -96,6 +96,31 @@ modalCloses.forEach((modalClose)=>{
 })
 
 //======================== Portfolio Swiper ======================//
+import 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 //========================== Testimonial ========================//
 
 
